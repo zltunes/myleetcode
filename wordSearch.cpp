1b4185344dc8e2c40bedf41e21df1363a27341a4
yiyunyao <nixingwuwei@gmail.com>
2014-07-25 19:42:59 +0800

    2014.7.25

--- wordSearch.cpp
+++ wordSearch.cpp
@@ -5,37 +5,75 @@ using namespace std;
 class Solution {
 public:
     bool exist(vector<vector<char> > &board, string word) {
+        isFound = false;
         if(word == "") return false;
+        pattern = word;
         this->height = board.size();
         if(height == 0) return false;
         this->width = board[0].size();
         if(width == 0) return false;
+        table = board;
         vector<pair<int, int>> position;
 
         char firstChr = word[0];
         for(int i = 0; i < height; ++i) {
             for(int j = 0; j < width; ++j) {
-                if(board[i][j] == firstChr) {
+                cout << table[i][j] << " ";
+                if(board[i][j] == firstChr)
                     position.push_back(make_pair(i, j));
-                } 
             }
+            cout << endl;
         }
-
         int index = 1;
-        while(index < word.length()) {
-            word
+        for(vector<pair<int, int>> :: iterator it = position.begin(); it != position.end(); ++it) {
+            cout << "begin with :" << it->first << "" << it->second << " " << table[it->first][it->second] << endl;
+            if(checkPattern(*it, 1) == true )
+                return true;
         }
+        return false;
     }
-    bool checkNext(pair<int, int> pos, int direction) {
+    bool checkPattern(pair<int, int>pos, int index) {
+        bool next[4];
+        vector<pair<int, int>> posStack;
+        next[0] = checkNext(pos, 0, index, posStack);
+        next[1] = checkNext(pos, 1, index, posStack);
+        next[2] = checkNext(pos, 2, index, posStack);
+        next[3] = checkNext(pos, 3, index, posStack);
+        return next[0] || next[1] || next[2] || next[3];
+    }
+    bool checkNext(pair<int, int> pos, int direction, int index, vector<pair<int, int>> &posStack) {
+        if(isFound) return true;
+        if(index >= pattern.size()) {
+            isFound = true;
+            return true;
+        }
         pos.first += dir[direction].y;
         pos.second += dir[direction].x;
+        for(vector<pair<int, int>> :: iterator it = posStack.begin(); it != posStack.end(); ++it) {
+            if(it->second == pos.second && it->first == pos.first) {
+                return false;
+            }
+        }
+        cout << "(" << pos.first << "," << pos.second << "), index is " << index << endl;
         if(pos.first < 0 || pos.first >= this->height) {
             return false; 
         }
         if(pos.second < 0 || pos.second >= this->width) {
             return false;
         }
-
+        if(table[pos.first][pos.second] == pattern[index]) {
+            posStack.push_back(pos);
+            cout << "pattern matched : " << pattern[index] << endl;
+            bool next[4];
+            next[0] = checkNext(pos, 0, index + 1, posStack);
+            next[1] = checkNext(pos, 1, index + 1, posStack);
+            next[2] = checkNext(pos, 2, index + 1, posStack);
+            next[3] = checkNext(pos, 3, index + 1, posStack);
+            posStack.pop_back();
+            if(next[0] || next[1] || next[2] || next[3])
+                return true;
+        }
+        return false;
     }
     Solution() {
         dir[0].x = 1;
@@ -52,15 +90,15 @@ private:
         int x, y;
     } dir[4];
     int height, width;
+    vector<vector<char>> table;
+    string pattern;
+    bool isFound;
 };
 
 
 int main() {
-    vector<vector<char>> vec;
-    vector<char> inner_vec;
-    inner_vec.push_back('1');
-    vec.push_back(inner_vec);
+    vector<vector<char>> vec = {{'a', 'b', 'c', 'e'}, {'s', 'f', 'c', 's'}, {'a', 'd', 'e', 'e'}};
     Solution sol;
-    sol.exist(vec, "");
+    cout << sol.exist(vec, "see") << endl;
     return 0;
 }
